a859a139e7a1283d28af0af3e9882cd82f98a345
John Barksdale <jdbarks@gmail.com>
2019-01-04 17:32:56 -0600

    Handled the case of if it is the birthday month

--- program.py
+++ program.py
@@ -14,7 +14,22 @@ birth_date = datetime.datetime(year=int(date_of_birth[2]),
 
 todays_date = datetime.datetime.now()
 
-days_alive = todays_date - date_of_birth
-print(delta_birth_date.days)
+days_alive = todays_date - birth_date
+print(days_alive.days)
 
-print(f'It looks like you were born on {date_of_birth.strftime("%m/%d/%Y")}')
+print(f'It looks like you were born on {birth_date.strftime("%m/%d/%Y")}')
+if todays_date.month == birth_date.month:
+    if todays_date.day == birth_date.day:
+        print(f'Happy Birthday!  You\'ve been on this planet for '
+              f'{days_alive.days} days!')
+    elif todays_date.day < birth_date.day:
+        days_until_birthday = birth_date.replace(
+            year=todays_date.year) - todays_date
+        print(f'Looks like your birthday is in {days_until_birthday.days} days.')
+        print('Hope you\'re looking forward to it!')
+    else:
+        days_until_birthday = birth_date.replace(
+            year=(todays_date.year + 1)) - todays_date
+        print(
+            f'Looks like your birthday is in {days_until_birthday.days} days.')
+        print('Hope you\'re looking forward to it!')
